fa15a24f6bba25d4f74fd32e6b87fb1e81ef852d
abzianidze <g.abzianidze@fortunejack.com>
2025-01-12 00:59:29 +0400

    resize

--- utils/createApp.js
+++ utils/createApp.js
@@ -6,11 +6,23 @@ export const createApp = (width = 600, height = 600, color = '0x65d2eb') => {
     backgroundColor: color,
     resolution: window.devicePixelRatio,
     autoDensity: true,
+    backgroundAlpha: 0,
   })
   if (window.innerWidth < width) {
     app.renderer.resize(window.innerWidth, window.innerWidth)
     app.stage.scale.set(window.innerWidth / width)
   }
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth < width) {
+      app.renderer.resize(window.innerWidth, window.innerWidth)
+      app.stage.scale.set(window.innerWidth / width)
+    } else {
+      app.renderer.resize(width, height)
+      app.stage.scale.set(1)
+    }
+  })
+
   globalThis.__PIXI_APP__ = app
   root.appendChild(app.view)
   return app
